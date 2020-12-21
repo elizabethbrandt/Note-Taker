b69a5bdec457fb9f1dff92d3a55823daa018fb96
Elizabeth Brandt <ebrandt8@uw.edu>
2020-12-20 23:28:17 -0800

    starting express server created

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get("/api/notes", function(req, res) {
+
+    // Use the fs module to read the `db.json` file
+    fs.readFile("db.jason")
+
+    // THEN parse the file contents with JSON.parse to get the real data
+
+    // THEN send the parsed data back to the client with res.json()
+
+})
+
+app.post("/api/notes", function(req, res) {
+
+    // Access the posted data in `req.body`
+    
+    // Use the fs module to read the `db.json` file
+    fs.readFile("db.jason")
+    
+    // THEN parse the file contents with JSON.parse to get the real data
+    
+    // Push the `req.body` to the array list
+
+    // JSON.stringify() the array list back into a JSON string
+
+    // THEN save the contents back to the `db.json` file with the fs module
+
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+
+    // Access the :id from `req.params.id`
+    
+    // Use the fs module to read the `db.json` file
+    fs.readFile("db.jason")
+
+    // THEN parse the file contents with JSON.parse to get the real data
+
+    // OPTION A
+        // Find the matching index using .findIndex() method
+        // Remove the target element using the .splice() method
+
+    // OPTION B
+        // Use the array.filter() method to filter out the matching element
+        // myArray = myArray.filter(element => element.id !== req.params.id);
+
+    // Return any type of success message
+
+})
+
+app.get("/notes", function(req, res) {
+    
+    // return the contents of the notes.html
+    res.sendFile(path.join(__dirname, "notes.html"));
+    
+});
+
+app.get("*", function(req, res) {
+
+    //return the contents of the index.html
+    res.sendFile(path.join(__dirname, "index.html"));
+
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
